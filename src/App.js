b20632fb048801ce8a273f80083c143070b9bc61
selehadin-cyber <sele4416@gmail.com>
2022-03-14 21:34:29 +0300

    pokemon api with image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [nameArray, setNameArray] = useState([]);
+  const [rawData, setRawData] = useState("");
+  const [imageIndex, setImageIndex] = useState(1)
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20")
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setRawData(JSON.stringify(data), null, 2);
+        setNameArray(data.results);
+        console.log(nameArray);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select name="test" id="test">
+        {nameArray.map((e, idx) => <option key={idx} value={idx + 1} onClick={(e) => {setImageIndex(e.target.value)}}>{e.name}</option>)}
+      </select>
+      {/*<pre>{rawData}</pre>*/}
+      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imageIndex}.png`} alt="pokemon" />
     </div>
   );
 }
