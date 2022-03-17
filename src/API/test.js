8a5acb6b12445169c709c203a2ee6ac7e4297567
selehadin-cyber <sele4416@gmail.com>
2022-03-17 22:18:09 +0300

    post interview

---
+++ src/API/test.js
@@ -0,0 +1,3 @@
+export default function Add() {
+    console.log("imported add function")
+}
--- src/App.js
+++ src/App.js
@@ -1,84 +1,82 @@
 import React, { useState, useEffect } from "react";
+import Add from "./API/test";
 import "./App.css";
 
+const paintData = {
+  projectTypes: [{ name: "Interior" }, { name: "Exterior" }],
+
+  sectors: [
+    { name: "Hospital", costMultiplier: 1.1 },
+    { name: "School", costMultiplier: 1 },
+    { name: "Housing", costMultiplier: 0.9 },
+    { name: "Hall", costMultiplier: 1 },
+    { name: "Park", costMultiplier: 0.8 },
+  ],
+
+  products: [
+    { name: "Glidden Pro", price: 0.4, redecorationCycle: 5 },
+    { name: "Mustang Speedwall", price: 0.1, redecorationCycle: 1 },
+    { name: "Lifemaster Accents", price: 0.8, redecorationCycle: 9 },
+    { name: "Weatherward Max", price: 0.6, redecorationCycle: 3 },
+    { name: "Acrylic Diamond", price: 0.8, redecorationCycle: 8 },
+  ],
+
+  colour: [
+    { name: "Just White", hex: "#FFFFFF" },
+    { name: "Elephant's breath", hex: "#d0c2b4" },
+    { name: "Crown", hex: "#d64b30" },
+    { name: "Julie's Dream", hex: "#efe3d8" },
+    { name: "Dead Salmon", hex: "#c29e8f" },
+    { name: "Nacho Chese", hex: "#ffc56d" },
+    { name: "Little Greene", hex: "#7f5c56" },
+  ],
+
+  projectType_sector: [
+    { type: "Interior", sector: "Hospital" },
+    { type: "Exterior", sector: "Hospital" },
+    { type: "Interior", sector: "School" },
+    { type: "Exterior", sector: "School" },
+    { type: "Interior", sector: "Housing" },
+    { type: "Exterior", sector: "Housing" },
+    { type: "Interior", sector: "Hall" },
+    { type: "Exterior", sector: "Park" },
+  ],
+
+  sector_product: [
+    { sector: "Hospital", product: "Glidden Pro" },
+    { sector: "School", product: "Glidden Pro" },
+    { sector: "Hall", product: "Glidden Pro" },
+    { sector: "School", product: "Mustang Speedwall" },
+    { sector: "Housing", product: "Mustang Speedwall" },
+    { sector: "Park", product: "Mustang Speedwall" },
+    { sector: "Hospital", product: "Lifemaster Accents" },
+    { sector: "Housing", product: "Weatherward Max, Hall and Park" },
+    { sector: "Hall", product: "Weatherward Max, Hall and Park" },
+    { sector: "Park", product: "Weatherward Max, Hall and Park" },
+    { sector: "Hospital", product: "Acrylic Diamond" },
+    { sector: "School", product: "Acrylic Diamond" },
+    { sector: "Housing", product: "Acrylic Diamond" },
+    { sector: "Hall", product: "Acrylic Diamond" },
+    { sector: "Park", product: "Acrylic Diamond" },
+  ],
+};
 function App() {
-  const [nameArray, setNameArray] = useState([]);
-  const [rawData, setRawData] = useState("");
-  const [imageIndex, setImageIndex] = useState(1);
-  const [userName, setUserName] = useState([]);
-  const [userPicURL, setUserPicURL] = useState([]);
-  const [dataArray, setDataArray] = useState([]);
-  const [counter, setCounter] = useState(1);
+  const [userSelectedProjectType, setUserSelectedProjectType] = useState("");
 
-  const fetchPokemonData = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20")
-      .then((res) => res.json())
-      .then((data) => {
-        //console.log(data);
-        setRawData(JSON.stringify(data), null, 2);
-        setNameArray(data.results);
-        console.log(nameArray);
-      });
-  };
-  const url = `https://randomuser.me/api/?results=${counter}`;
-  const fetchUserData = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setDataArray(data.results);
-        /* setUserName(
-          data.results[counter].name.title +
-            " " +
-            data.results[counter].name.first +
-            " " +
-            data.results[counter].name.last
-        );
-        setUserPicURL(data.results[0].picture.large) */
-      });
-  };
-
-  useEffect(() => {
-    fetchPokemonData();
-    fetchUserData();
-    console.log(counter)
-  }, [counter]);
   return (
-    <div className="App">
-      <select name="test" id="test">
-        {nameArray.map((e, idx) => (
+    <div>
+      <h1>TEST</h1>
+      <select name="projectType" id="projectType">
+        {paintData.projectTypes.map((e, idx) => (
           <option
-            key={idx}
-            value={idx + 1}
-            onClick={(e) => {
-              setImageIndex(e.target.value);
-            }}
+            value="" key={idx}
+            onClick={() => {setUserSelectedProjectType(e.name)
+            console.log(userSelectedProjectType)}}
           >
             {e.name}
           </option>
         ))}
       </select>
-      {/*<pre>{rawData}</pre>*/}
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imageIndex}.png`}
-        alt="pokemon"
-      />
-      <button
-        onClick={() => {
-          setCounter(counter => counter + 1);
-        }}
-      >
-        see more users
-      </button>
-      {dataArray.map((e) => (
-        <div>
-          <div>{e.name.title +
-            " " +
-            e.name.first +
-            " " +
-            e.name.last}</div>
-            <img src={e.picture.large} alt="" />
-        </div>
-      ))}
     </div>
   );
 }
